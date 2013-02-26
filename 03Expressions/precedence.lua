de2d786e904551283fe1205552d297eebabfe443
Patrick Elsen <pelsen.vn@gmail.com>
2013-02-26 17:53:21 +0100

    updated precedence.lua

--- 03Expressions/precedence.lua
+++ 03Expressions/precedence.lua
@@ -4,6 +4,10 @@
 -- thus, the parentheses in the following statement
 -- are not really necessary:
 -- (x and y and (not z)) or ((not y) and x)
+-- however, they should probably be kept because
+-- they show what is evaluated first (since most
+-- people don't know the operator precedence by
+-- hard)
 
 -- proof
 print("not true and 'false' or 'true'  --> " .. (not true and 'false' or 'true'))
