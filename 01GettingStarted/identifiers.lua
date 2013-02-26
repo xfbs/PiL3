e179b4f0d301f762e5950812108636e59d0e4a1f
Patrick Elsen <pelsen.vn@gmail.com>
2013-02-26 17:06:16 +0100

    added identifiers.lua

---
+++ 01GettingStarted/identifiers.lua
@@ -0,0 +1,10 @@
+-- file: identifiers.lua
+
+-- which are valid identifiers?
+print("until?\t--> nope, question marks are not allowed")
+print("end\t--> nope, reserved keyword")
+print("nil\t--> nope, reserved type name")
+print("___\t--> valid")
+print("_end\t--> valid")
+print("End\t--> valid")
+print("NULL\t--> valid")
