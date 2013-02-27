8aee37227ba416f13d4e3f2772f399ccae2f5096
Patrick Elsen <pelsen.vn@gmail.com>
2013-02-27 14:48:20 +0100

    added concatenate.lua

---
+++ 05Functions/concatenate.lua
@@ -0,0 +1,25 @@
+-- file: concatenate.lua
+
+-- function to concatenate all the strings
+-- passed as arguments
+function concatenate(...)
+    -- put the variable arguments into an array
+    args = table.pack(...)
+
+    -- complain if there are no arguments
+    if args.n == 0 then
+        return "concatenate: no arguments!"
+    end
+
+    -- concatenate arguments
+    str = tostring(args[1])
+    for i = 2, args.n do
+        str = str .. ' ' .. tostring(args[i])
+    end
+    return str
+end
+
+-- some examples
+print(concatenate('This', 'is', 'a', 'concatenated', 'string.'))
+print(concatenate(true))
+print(concatenate())
