38cbf02bf9bd793f92d81169ee55c984769d14b5
Patrick Elsen <pelsen.vn@gmail.com>
2013-02-28 18:36:58 +0100

    added library loading examples (chapter 1)

---
+++ 01GettingStarted/examples/dofile.lua
@@ -0,0 +1,3 @@
+dofile("lib1.lua") -- load your library
+n = norm(3.4, 1.0)
+print(twice(n)) --> 7.0880180586677
---
+++ 01GettingStarted/examples/lib1.lua
@@ -0,0 +1,7 @@
+function norm(x, y)
+    return (x^2 + y^2)^0.5
+end
+
+function twice(x)
+    return 2*x
+end
