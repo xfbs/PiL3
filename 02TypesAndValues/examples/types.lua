8e2b7413d6bc8ac8bff62bbc72f5cc7e10c56cf0
Patrick Elsen <pelsen.vn@gmail.com>
2013-02-28 19:01:33 +0100

    added types example (chapter 2)

---
+++ 02TypesAndValues/examples/types.lua
@@ -0,0 +1,10 @@
+-- file: types.lua
+
+-- print the types of that values
+print('"Hello world" --> '..type("Hello world"))
+print('10.4*3        --> '..type(10.4*3))
+print('print         --> '..type(print))
+print('type          --> '..type(type))
+print('true          --> '..type(true))
+print('nil           --> '..type(nil))
+print('type(X)       --> '..type(type(X)))
