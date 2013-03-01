b420230e75e6949902102ee07fa28c1c280cc52b
Patrick Elsen <pelsen.vn@gmail.com>
2013-03-01 23:51:52 +0100

    added fromtostep.lua

---
+++ 07Iterators/fromtostep.lua
@@ -0,0 +1,29 @@
+-- file: fromtostep.lua
+
+-- fromto iterator generator with step variable
+function fromto(min, max, step)
+    -- iterator function, takes a state (table)
+    -- and the current value
+    local function iter(state, current)
+        -- increase current by the step specified
+        -- in the invariant state
+        current = current + state.step
+
+        -- check to see if we have passed the max
+        -- and return nil if so to end the loop
+        if not (current > state.max) then
+            return current
+        else
+            return nil
+        end
+    end
+
+    -- return iterator and crease the invariant
+    -- state
+    return iter, {max=max, step=step}, min-1
+end
+
+-- test the iterator in a for loop
+for i in fromto(0, 15, 3) do
+    print("fromtostep(0, 15, 3): " .. i)
+end
