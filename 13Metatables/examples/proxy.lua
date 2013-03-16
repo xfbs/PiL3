437dcd1ecb5c17cea18ff28a662193cf5c36aee3
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-16 14:03:04 +0100

    updated proxy example to be better (chapter 13)

--- 13Metatables/examples/proxy.lua
+++ 13Metatables/examples/proxy.lua
@@ -1,37 +1,39 @@
 -- file: proxy.lua
 -- a proxy for a table to track access
 
-t = {"a", "b", "c"}  -- original table
-
--- keep a private access to the original table
-local _t = t
-
--- create proxy
-t = {}
+-- create private index
+local index = {}
 
 -- create metatable
 local mt = {
     __index = function(t, k)
         print("*access to element " .. tostring(k))
-        return _t[k]    -- access to the original table
+        return t[index][k]    -- access to the original table
     end,
 
     __newindex = function(t, k, v)
         print("*update of element " .. tostring(k) ..
             " to " .. tostring(v))
-        _t[k] = v   -- update original table
+        t[index][k] = v   -- update original table
     end,
 
     -- is called when using pairs() with the proxy
-    __pairs = function()
-        return function(_, k)
-            return next(_t, k)
-        end
+    __pairs = function(t)
+        return function(t, k)
+            return next(t[index], k)
+        end, t
     end
 }
 
-setmetatable(t, mt)
+function track(t)
+    local proxy = {}
+    proxy[index] = t
+    setmetatable(proxy, mt)
+    return proxy
+end
+
+t = track{"a", "b", "c"}
 
 for k, v in pairs(t) do
-    print(k .." --> " .. v)
+    print(tostring(k) .. " --> " .. tostring(v))
 end
