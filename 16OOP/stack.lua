500cb88807ce6940925454364ee79515d025bf04
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-17 22:21:43 +0100

    wrote stack class (chapter 16)

---
+++ 16OOP/stack.lua
@@ -0,0 +1,40 @@
+-- file: stack.lua
+-- a simple stack class
+
+Stack = {pointer = 0}
+
+function Stack:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function Stack:push(v)
+    -- increment stack pointer
+    self.pointer = self.pointer + 1
+
+    -- save value
+    self[self.pointer] = v
+end
+
+function Stack:isempty()
+    -- if stack pointer is zero, the
+    -- stack is empty
+    return self.pointer == 0
+end
+
+function Stack:pop()
+    if self:isempty() then return nil end
+
+    -- get value
+    local value = self[self.pointer]
+
+    -- set to nil (for the gc)
+    self[self.pointer] = nil
+
+    -- decrement stack pointer
+    self.pointer = self.pointer - 1
+
+    return value
+end
