de45d3b24ebc4d467ede30d81ccce327c25a5a04
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-18 21:24:26 +0100

    working on refactoring chapter 2

--- 02TypesAndValues/examples/floaterror.lua
+++ 02TypesAndValues/examples/floaterror.lua
@@ -1,8 +1,19 @@
+#!/usr/bin/env lua
 -- file: floaterror.lua
+-- shows one of the flaws of floating-point algebra
+-- with fixed precision: not all numbers have an
+-- exact representation, which can lead to funny
+-- results in some cases.
 
--- this should be 0, but it's not.
-num = 12.7-20+7.3
-eqn = "12.7-20+7.3"
+-- here, we calculate 12.7 - 20 + 7.3, which should
+-- be exactly zero, but since both 12.7 and 7.3 do
+-- not have an exact representation, there is some
+-- rounding involved which causes the result to not
+-- be zero (~8x10^-16 on my machine)
+num = 12.7 - 20 + 7.3
+eqn = "12.7 - 20 + 7.3"
 
-print(eqn..'==0 is '..tostring(num==0)..', but it shoule be true!')
-print(eqn..'='..num)
+-- print if the number is equal to zero (probably
+-- not) and what it is
+print(eqn..' == 0 is '..tostring(num==0)..', but it should be true!')
+print(eqn..' = '..num)
--- 02TypesAndValues/examples/strings.lua
+++ 02TypesAndValues/examples/strings.lua
@@ -1,10 +1,16 @@
+#!/usr/bin/env lua
 -- file: strings.lua
--- some string examples
+-- some examples of string operations
 
+-- string.gsub() replaces a given substring of
+-- a string with another one
 a = "Some random string"
 b = string.gsub(a, "random", "deterministic")
 
 print('a = "' .. a .. '"')
 print('b = "' .. b .. '"')
+
+-- the length operator (#) returns the length
+-- of the string
 print('#a = ' .. #a)
 print('#b = ' .. #b)
