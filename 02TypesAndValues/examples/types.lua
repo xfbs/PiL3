7de36467105d520619347c72e97c125b0ffe47cc
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-18 21:30:25 +0100

    refactored types.lua (chapter 2)

--- 02TypesAndValues/examples/types.lua
+++ 02TypesAndValues/examples/types.lua
@@ -1,10 +1,13 @@
+#!/usr/bin/env lua
 -- file: types.lua
+-- shows which variables have which types
+-- (determined with the type() function)
 
--- print the types of that values
-print('"Hello world" --> '..type("Hello world"))
-print('10.4*3        --> '..type(10.4*3))
-print('print         --> '..type(print))
-print('type          --> '..type(type))
-print('true          --> '..type(true))
-print('nil           --> '..type(nil))
-print('type(X)       --> '..type(type(X)))
+-- print the types of those values
+print('"Hello world"', '--> '..type("Hello world"), "-- anything enclosed in quotes is a string")
+print('10.4*3\t', '--> '..type(10.4*3), "-- ints and floats have the same type in lua")
+print('print\t',  '--> '..type(print), "-- functions are also variables with a type")
+print('type\t',   '--> '..type(type), "-- type() is a function just like all the others")
+print('true\t',   '--> '..type(true))
+print('nil\t',    '--> '..type(nil))
+print('type(X)\t','--> '..type(type(X)), "-- type() always returns a string")
