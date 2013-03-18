d391481bbe706f4d7fe9dec68687250684f41cf2
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-18 21:48:45 +0100

    refactored embedding.lua (chapter 2)

--- 02TypesAndValues/embedding.lua
+++ 02TypesAndValues/embedding.lua
@@ -1,18 +1,24 @@
+#!/usr/bin/env lua
 -- file: embedding.lua
+-- Exercise 2.4: How can you embed the following piece
+-- of XML as a string in Lua? Show at least two
+-- different ways.
 
--- two ways to embed XML data
--- into Lua. First uses a long
--- string literal while the second
--- one uses the more compact
--- escaped string version.
+-- you can use a long string literal
+-- -> no need to escape stuff
+-- -> readable
+-- -> ugly if the line this is on is indented
 xmldata1 = [=[
 <![CDATA[
   Hello World
 ]]>]=]
 
+-- an escaped string
+-- -> full control over what's in string
+-- -> not too readable
+-- -> compact
 xmldata2 = "<![CDATA[\n  Hello World\n]]>"
 
 -- these two forms are identical,
--- but the former is more readable.
 print("xmldata1 == xmldata2 --> " .. ((xmldata1==xmldata2) and "true" or "false"))
 print(xmldata1)
