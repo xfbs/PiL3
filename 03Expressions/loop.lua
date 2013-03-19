700c4017c9a3d3b193b6fb5ee28c2a78e8d2b588
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-19 10:22:20 +0100

    refactored loop.lua

--- 03Expressions/loop.lua
+++ 03Expressions/loop.lua
@@ -1,20 +1,16 @@
 -- file: loop.lua
+-- Exercise 3.1: What will the following program
+-- print?
+--     for i = -10, 10 do
+--       print(i, i % 3)
+--      end
 
--- an example of a loop
-print("What will this print:")
-print("\tfor i = -10, 10 do")
-print("\t  print(i, i % 3)")
-print("\tend")
-
-print([[
-As the for loop progresses from -10 to 10,
-The values of `i` are printed at each step,
-along with the value of `i mod 3`. -10 mod
-3 is 2, so it will start there, and either 
-increase or go to 0 (the latter if it is
-at 2).
-]])
-
+-- As the for loop progresses from -10 to 10,
+-- The values of `i` are printed at each step,
+-- along with the value of `i mod 3`. -10 mod
+-- 3 is 2, so it will start there, and either 
+-- increase or go to 0 (the latter if it is
+-- at 2).
 for i = -10, 10 do
     print(i, i % 3)
 end
