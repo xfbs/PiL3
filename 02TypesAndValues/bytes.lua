968270e73e1c211d3406adc4b216120ebab21eec
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-19 07:37:24 +0100

    refactored bytes.lua

--- 02TypesAndValues/bytes.lua
+++ 02TypesAndValues/bytes.lua
@@ -1,6 +1,15 @@
+#!/usr/bin/env lua
 -- file: bytes.lua
+-- Exercise 2.5: Suppose you need to format a long sequence
+-- of arbitrary bytes as a string literal in Lua. How would
+-- you do it? Consider issues like readability, maximum line
+-- length, and performance.
 
--- best way to embed data into lua
+-- In such a case, I would use a long string literal. The
+-- advantage here is that it doesn't need much escaping, but 
+-- one can still enter arbitrary binary data. It can be as
+-- long as necessary, it can span multiple lines and it
+-- should be quite fast.
 data = [[
 fklshfs lkfhjsdfsdf sdklfshghsfglksdfh...
 ]]
