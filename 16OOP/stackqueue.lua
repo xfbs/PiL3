90446ebedb5e3cf501bae44850fa140058e47c3b
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-20 13:17:26 +0100

    refactored stackqueue.lua

--- 16OOP/stackqueue.lua
+++ 16OOP/stackqueue.lua
@@ -1,28 +1,28 @@
+#!/usr/bin/env lua
 -- file: stackqueue.lua
+-- Exercise 16.2: Implement a class StackQueue as a subclass of
+-- Stack. Besides the inherited methods, add to this class a method
+-- insertbottom, which inserts an element as the bottom of the
+-- stack. (This method allows us to use objects of this class as
+-- queues.)
 
-dofile("stack.lua") -- load the stack class
+-- load the stack class
+dofile("stack.lua")
+
+-- create a subclass of Stack with a new pointer, bottompointer,
+-- which keeps track of where the lowest element in the queue
+-- is.
 StackQueue = Stack:new{bottompointer=0}
 
-function StackQueue:pop()
-    -- make sure it's not empty
-    if self.pointer == self.bottompointer then return nil end
-
-    -- save value
-    local value = self[self.pointer]
-
-    -- delete from queue
-    self[self.pointer] = nil
-
-    -- decrease stack pointer
-    self.pointer = self.pointer - 1
-
-    return value
-end
-
+-- due to the new bottompointer, we need a new test for the 
+-- stackqueue being empty. this is true if the bottompointer and
+-- the stack pointer are equal.
 function StackQueue:isempty()
     return self.pointer == self.bottompointer
 end
 
+-- method to insert data at the bottom (so as to be able to use
+-- the class as a queue)
 function StackQueue:insertbottom(v)
     -- insert value
     self[self.bottompointer] = v
