3445b2cab65f0369724ab2b83105c68bb6fc853b
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-20 14:52:49 +0100

    refactored polynomials.lua (chapter 3)

--- 03Expressions/polynomials.lua
+++ 03Expressions/polynomials.lua
@@ -1,4 +1,9 @@
+#!/ust/bin/env lua
 -- file: polynomials.lua
+-- Exercise 3.3: We can represent a polynomial in Lua as a list
+-- of coefficients, such as {a0, a1, ..., an}.
+-- Write a function that receives a polynomial (represented as
+-- a table) and a value for x and returns the polynomial value.
 
 -- coefficients are stored in a Lua array,
 -- with the first array member representing
