ebd332fc3dd2c0542726e9e707d10ed25376ab64
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-20 14:58:44 +0100

    refactored precedence.lua (chapter 3)

--- 03Expressions/precedence.lua
+++ 03Expressions/precedence.lua
@@ -1,4 +1,9 @@
+#!/usr/bin/env lua
 -- file: precedence.lua
+-- Exercise 3.6: Consider the following expression:
+--     (x and y and (not z)) or ((not y) and x)
+-- Are the parentheses necessary? Would you recommend
+-- their use in that expression?
 
 -- operator precedence: not > and > or
 -- thus, the parentheses in the following statement
@@ -13,7 +18,7 @@
 print("not true and 'false' or 'true'  --> " .. (not true and 'false' or 'true'))
 print("not false and 'false' or 'true' --> " .. (not false and 'false' or 'true'))
 
--- first the not is evaluated (highes precedence),
+-- first the not is evaluated (highest precedence),
 -- then the and part is evaluated (below not but higher than or),
 -- finally the or part is evaluated (lowest precedence)
 
