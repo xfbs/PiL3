5c943a9bae049d0197b04c2a5af2166e3b0c41a2
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-20 15:06:58 +0100

    refactored excapes.lua (chapter 3)

--- 03Expressions/escapes.lua
+++ 03Expressions/escapes.lua
@@ -1,12 +1,29 @@
+#!/usr/bin/env lua
 -- file: escapes.lua
+-- Exercise 3.8: Suppose that you want to create a table
+-- that associates each escape sequence for strings with
+-- it's meaning. How could you write a constructor for
+-- that table?
 
--- a table that can be used to look up
--- string escapes
+-- We want to use the table as a kind of associative map,
+-- so there are two constructor types that we can consider:
+--   Record constructor:  {a="a", b="b", ...}
+--   General constructor: {["a"]="a", ["b"]="b", ...}
+-- The record constructor has one catch: it only works if
+-- the keys are valid identifiers. Escape sequences start
+-- with backslashes, which is not a valid character for an
+-- identifier, meaning that we cannot use this constructor.
+-- Even if we omit the backslashes it won't work because
+-- there are escape sequences (like \0) that, after
+-- stripping the backslash, start with digits, which also
+-- aren't supported. Thus, we should use the general
+-- constructor syntax for this:
 escapes = {
     ["\n"] = "newline",
     ["\r"] = "carriage return",
     ["\a"] = "alert",
-    ["\0"] = "null character"}
+    ["\0"] = "null character"
+}
 
 -- show some examples
 print("\\n --> " .. escapes["\n"])
