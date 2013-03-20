52e11bc70b842691e165448df61fa573b91cfa15
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-03-20 15:01:55 +0100

    refactored days.lua (chapter 3)

--- 03Expressions/days.lua
+++ 03Expressions/days.lua
@@ -1,20 +1,19 @@
+#!/usr/bin/env lua
 -- file: days.lua
+-- Exercise 3.7: What will the following print?
+--     sunday = "monday"; monday = "sunday"
+--     t = {sunday = "monday", [sunday] = monday}
+--     print(t.sunday, t[sunday], t[t.sunday])
 
-print("What will the following print?")
-print('  sunday = "monday"; monday = "sunday"')
-print('  t = {sunday = "monday", [sunday] = monday}')
-print('  print(t.sunday, t[sunday], t[t.sunday]')
-print()
-print('Answer: it should print "monday sunday sunday"')
-print('It is probably better understood if we show what')
-print('the constructor is really doing:')
-print('  t = {sunday = "monday", monday = "sunday"}')
-print()
-print('By this logic, the print statement should be more clear:')
-print('  print(t.sunday, t.monday, t.monday)')
-print()
-print('Here is the actual output:')
+-- Answer: it should print "monday sunday sunday"
+-- It is probably better understood if we show what
+-- the constructor is really doing:
+--     t = {sunday = "monday", monday = "sunday"}
 
+-- By this logic, the print statement should be more clear:
+--     print(t.sunday, t.monday, t.monday)
+
+-- Here is the actual output:
 sunday = "monday"
 monday = "sunday"
 t = {sunday = "monday", [sunday] = monday}
