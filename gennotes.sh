dcebb309d96cc5fea43af8289377f4530be6d1da
Patrick M. Elsen <pelsen.vn@gmail.com>
2013-05-14 10:45:27 +0200

    added header to gennotes script

--- gennotes.sh
+++ gennotes.sh
@@ -6,6 +6,17 @@
 # empty out the global notes.markdown
 echo -n > notes.markdown
 
+# add header
+echo "Programming in Lua Notes
+========================
+
+Programming in Lua gives a solid base for any programmer who wants to use
+Lua. It covers all aspects of Lua—from the basics to its API with C—explaining
+how to make good use of its features and giving numerous code examples.
+The book is targeted at people with some programming background, but it
+does not assume any prior knowledge about Lua or other scripting languages.
+" >> notes.markdown
+
 # compile it
 for note in ./*/notes.markdown; do
     cat $note >> notes.markdown;
