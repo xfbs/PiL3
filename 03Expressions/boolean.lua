ff228714e7555030b9dfa95249a391d5548ded9e
Jakukyo Friel <weakish@gmail.com>
2014-02-07 19:05:22 +0800

    Exe3.5: I <3 one liner

--- 03Expressions/boolean.lua
+++ 03Expressions/boolean.lua
@@ -4,22 +4,8 @@
 -- without using the type function?
 
 -- check if a variable is a boolean
-function is_boolean(v)
-    -- comparisons are false if the types are different
-    if v == true then
-        -- v is true and thus it's boolean
-        return true
-    elseif v == false then
-        -- v is false and thus it's boolean
-        return true
-    else
-        -- if it's neither true not false
-        -- it can't be boolean (or we have 
-        -- alpha particles affecting the
-        -- hardware)
-        return false
-    end
-end
+-- comparisons are false if the types are different
+function is_boolean(v) return a == true or a == false end
 
 -- test the function with some examples
 num = 123
